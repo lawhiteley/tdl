910525a0873de786a16c8bc6b73121093e6ad831
Luke Whiteley <luke@whiteley.io>
2020-03-22 02:15:19 +0000

    Updates plugin metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,10 @@ plugins {
     `java-gradle-plugin`
     `maven-publish`
 
+    id("com.gradle.plugin-publish") version "0.10.1"
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
-
-description = "Reflects Gradle test results (Red/Green) in a given Philips Hue room"
-group = "io.whiteley.luke"
-version = "0.0.4-SNAPSHOT"
 
 repositories {
     jcenter()
@@ -24,15 +21,6 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
-gradlePlugin {
-    plugins {
-        create("tdl") {
-            id = "io.whiteley.luke.tdl"
-            implementationClass = "io.whiteley.luke.TdlPlugin"
-        }
-    }
-}
-
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -46,3 +34,23 @@ tasks {
         useJUnitPlatform()
     }
 }
+
+gradlePlugin {
+    plugins {
+        create("tdl") {
+            id = "io.whiteley.luke.tdl"
+            displayName = "tdl"
+            description = "Reflects Gradle test results (Red/Green) in a given Philips Hue room"
+            implementationClass = "io.whiteley.luke.TdlPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/lawhiteley/tdl"
+    vcsUrl = "https://github.com/lawhiteley/tdl"
+    tags = listOf("iot", "philips", "hue", "kotlin", "testing", "tdd", "gradle")
+}
+
+group = "io.whiteley.luke"
+version = "1.0.0"
